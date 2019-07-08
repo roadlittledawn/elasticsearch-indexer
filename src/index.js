ef8cf7b9cbf5a233cea02fa2ba084ebc97a975d5
Clinton <clinton.langosch@gmail.com>
2019-07-08 15:35:52 -0700

    add gitignore. up and running with simple script to hit custom endpoint in dev and add to elasticsearch endpoint i set up

--- src/index.js
+++ src/index.js
@@ -4,53 +4,49 @@ const fs = require('fs');
 
 const { log } = console;
 
-//Regex pattern for match integrations node links
-// \/docs\/integrations\/.*\/.*\/.*
+/*
+Workflow
+1. Hit custom JSON endpoint.
+2. Send each page record to experimental elasticsearch endpoint with fields per spec.
+
+*/
+
+
 const domain = 'https://docs.newrelic.com';
 
-const run = async () => {
-  const res = await fetch('https://docs.newrelic.com/docs/integrations?toc=true');
-  const body = await res.text();
-  const $ = cheerio.load(body);
 
-  // Let's echo out the title tag.
-  const title = $('title').text();
-  // log(title);
 
-  // Instantiate hrefs array
-  const hrefs = [];
-  // Only crawl node links per pattern test
-  $('a[href^="/docs/integrations"]').each(function handle() {
-    const href = $(this).attr('href');
-    if (/\/docs\/integrations\/.*\/.*\/.*/.test(href)) {
-      hrefs.push(href);
-    }
+const addToSearchIndex =  (json) => {
+  const { docsPages } =  json;
+  docsPages.forEach(element => {
+    element.docsPage.body = element.docsPage.body.replace(/(\r\n|\n|\r|\t)/gm,"");
+    const searchIndexPutEndpoint = 'https://search-nr-docs-index-bkyrkvszbu2hfn6sez4k2f7yle.us-west-2.es.amazonaws.com/docs/doc/'+element.docsPage.nodeId;
+    const resp = fetch(searchIndexPutEndpoint, {
+      method: 'put',
+      body:    JSON.stringify(element.docsPage),
+      headers: { 
+        'Content-Type': 'application/json',
+        'X-Phpshield-Key-Disable' : 'SP6ZwOVbe76L_ZcPjaW70g'
+     },
+    }).then(res => res.json()).then(json => log(json));
+    // log(element.docsPage.body);
   });
+};
+
+
+const performIndex = async () => {
+  const res = await fetch('https://docs-dev.newrelic.com/search-index', {
+    headers: { 
+      'Content-Type': 'application/json',
+      'X-Phpshield-Key-Disable' : 'SP6ZwOVbe76L_ZcPjaW70g'
+   },
+  }).then(res => res.json())
+  .then(json => addToSearchIndex(json));
 
 };
 
-const run2 = async () => {
-  // $(hrefs).each(function(i) {
-    // const doc = await fetch(domain+${hrefs[i]});
-    const doc = await fetch('https://docs.newrelic.com/docs/integrations/host-integrations/host-integrations-list/mysql-monitoring-integration');
-    const docBody = await doc.text();
-    const $ = cheerio.load(docBody);
-    $nodes_json = [];
 
-    $('table tr').each(function(i){
-      // log(`Table #${i}:`+$(this).text());
-      const name = $(this).children().first().text();
-      const description = $(this).children().last().text();
-      $node_json = {"title": name, "description":description};
-      $nodes_json.push($node_json);
-    });
-
-    log($nodes_json);
-
-    // log(`Link #${i}: ${hrefs[i]}`);
-
-};
 
 // Run this bad boy!
-run().catch(e => setImmediate(() => { throw e; }));
-run2().catch(e => setImmediate(() => { throw e; }));
+performIndex().catch(e => setImmediate(() => { throw e; }));
+// run2().catch(e => setImmediate(() => { throw e; }));
