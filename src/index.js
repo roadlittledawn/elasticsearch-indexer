0dba6a8480382fe19ce5a168b7410707b22e18fc
Clinton <clinton.langosch@gmail.com>
2019-07-09 12:46:56 -0700

    rewrite using aws sdk module with authentication
    had to set up admin user and get user’s access key and secret access key (only available at creation). wasn’t able to get aws sdk to read environment variable so had to export to terminal session per their docs. next todo here is to add pagination but i don’t necessarily want to guzzle all pages for over 2k nodes yet.

--- src/index.js
+++ src/index.js
@@ -1,44 +1,61 @@
 const fetch = require('node-fetch');
-const cheerio = require('cheerio');
-const fs = require('fs');
+const AWS = require('aws-sdk');
 
 const { log } = console;
 
-/*
-Workflow
-1. Hit custom JSON endpoint.
-2. Send each page record to experimental elasticsearch endpoint with fields per spec.
-*/
 
-const addToSearchIndex =  (json) => {
-  const { docsPages } =  json;
+var region = 'us-west-2'; // e.g. us-west-1
+var domain = 'search-nr-docs-index-bkyrkvszbu2hfn6sez4k2f7yle.us-west-2.es.amazonaws.com'; // e.g. search-domain.region.es.amazonaws.com
+var index = 'docs';
+var type = 'doc';
+
+async function indexDocument() {
+
+  var endpoint = new AWS.Endpoint(domain);
+  var request = new AWS.HttpRequest(endpoint, region);
+
+  var json = await getDocsJSON(3);
+  var { docsPages } =  json;
+
   docsPages.forEach(element => {
-    // Remove line break and spacing characters that the Drupal Views display spits out.
-    element.docsPage.body = element.docsPage.body.replace(/(\r\n|\n|\r|\t)/gm,"");
-    const searchIndexPutEndpoint = 'https://search-nr-docs-index-bkyrkvszbu2hfn6sez4k2f7yle.us-west-2.es.amazonaws.com/docs/doc/'+element.docsPage.nodeId;
-    const resp = fetch(searchIndexPutEndpoint, {
-      method: 'put',
-      body:    JSON.stringify(element.docsPage),
-      headers: { 
-        'Content-Type': 'application/json',
-        'X-Phpshield-Key-Disable' : 'SP6ZwOVbe76L_ZcPjaW70g'
-     },
-    }).then(res => res.json()).then(json => log(json));
-    // log(element.docsPage.body);
-  });
-};
+    request.method = 'PUT';
+    request.path = `/${index}/${type}/${element.docsPage.nodeId}`;
+    request.body = JSON.stringify(element.docsPage);
+    request.headers['host'] = domain;
+    request.headers['Content-Type'] = 'application/json';
+    // Content-Length is only needed for DELETE requests that include a request
+    // body, but including it for all requests doesn't seem to hurt anything.
+    request.headers["Content-Length"] = request.body.length;
 
+    var credentials = new AWS.EnvironmentCredentials('AWS');
+    var signer = new AWS.Signers.V4(request, 'es');
+    signer.addAuthorization(credentials, new Date());
 
-const performIndex = async () => {
-  const res = await fetch('https://docs-dev.newrelic.com/search-index', {
+    var client = new AWS.HttpClient();
+    client.handleRequest(request, null, function(response) {
+      console.log(response.statusCode + ' ' + response.statusMessage);
+      var responseBody = '';
+      response.on('data', function (chunk) {
+        responseBody += chunk;
+      });
+      response.on('end', function (chunk) {
+        console.log('Response body: ' + responseBody);
+      });
+    }, function(error) {
+      console.log('Error: ' + error);
+    });
+  })
+}
+
+async function getDocsJSON(page=1) {
+  const res = await fetch(`https://docs-dev.newrelic.com/search-index?page=${page}`, {
     headers: { 
       'Content-Type': 'application/json',
       'X-Phpshield-Key-Disable' : 'SP6ZwOVbe76L_ZcPjaW70g'
    },
-  }).then(res => res.json())
-  .then(json => addToSearchIndex(json));
+  });
+  if (!res.ok) throw new Error(`${res.statusCode}: ${res.statusText}`);
+  return res.json();
+}
 
-};
-
-// Run this bad boy!
-performIndex().catch(e => setImmediate(() => { throw e; }));
+indexDocument().catch(e => setImmediate(() => { throw e; }))
