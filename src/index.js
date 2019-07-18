8971e542ac2b3d5c98782d3c61bca10729948f65
Clinton <clinton.langosch@gmail.com>
2019-07-17 21:23:40 -0700

    add dotenv module to load .env variables for aws sdk to authenticate
    avoids having to export in terminal every time

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const fetch = require('node-fetch');
 const fs = require('fs');
 const AWS = require('aws-sdk');
+const dotenv = require('dotenv');
+dotenv.config();
 const crawlHistory = require('./crawl-history.js');
 
 const { log } = console;
